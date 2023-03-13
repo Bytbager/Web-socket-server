5b1980bf66ae394458d1c19ca42e82747526c0e8
Bytbager <sdgrfdghgfg@gmail.com>
2023-03-13 15:24:02 +0700

    develop Version 2.0

--- Source/libs/funcs.c
+++ Source/libs/funcs.c
@@ -1,27 +1,22 @@
 #include "funcs.h"
+
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 
 void parse_http(char *filename, int client_sock) {
     char buffer[BUFSIZ] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n";
     int readsiz = strlen(buffer);
     send(client_sock, buffer, readsiz, 0);
     FILE *page = fopen(filename, "r");
-    while((readsiz = fread(buffer, sizeof(char), BUFSIZ, page))!=0){
+    while ((readsiz = fread(buffer, sizeof(char), BUFSIZ, page)) != 0) {
         send(client_sock, buffer, readsiz, 0);
     }
     fclose(page);
 }
 
-HTTPreq new_http(void) {
-    return (HTTPreq) {
-        .method = {0},
-        .path = {0},
-        .protocol = {0}
-    };
-}
+HTTPreq new_http(void) { return (HTTPreq){.method = {0}, .path = {0}, .protocol = {0}}; }
 
 void parse_request(char *request, HTTPreq *req) {
     int i = 0, j = 0;
@@ -30,12 +25,12 @@ void parse_request(char *request, HTTPreq *req) {
         req->method[j] = request[i];
     }
     i++;
-    for (j=0;; i++, j++) {
+    for (j = 0;; i++, j++) {
         if (request[i] == ' ') break;
         req->path[j] = request[i];
     }
     i++;
-    for (j=0;; i++, j++) {
+    for (j = 0;; i++, j++) {
         if (request[i] == '\n') break;
         req->protocol[j] = request[i];
     }
--- Source/main.c
+++ Source/main.c
@@ -1,10 +1,11 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "libs/funcs.h"
 
 #define PORT 1234
@@ -18,76 +19,67 @@ int main(void) {
         perror("socket() error!");
         exit(-1);
     }
-    setsockopt(server_sock,SOL_SOCKET,SO_REUSEADDR, &(int) {1}, sizeof(int));
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_sock, (struct sockaddr *)&server_address, len) == -1){
+    if (bind(server_sock, (struct sockaddr *)&server_address, len) == -1) {
         perror("bind() error!");
         exit(-1);
     }
 
-    if (listen(server_sock, MAX_CONN) == -1){
+    if (listen(server_sock, MAX_CONN) == -1) {
         perror("listen() error!");
         exit(-1);
     }
     while (1) {
-    if ((client_sock = accept(server_sock, (struct sockaddr*)&server_address, &len)) == -1) {
-        perror("accept() error!");
-        exit(-1);
-    }
-    
-    recv(client_sock, recv_msg, sizeof(recv_msg), 0);
-    HTTPreq req = new_http();
-    parse_request(recv_msg, &req);
-    printf("%s %s %s\n",req.method,req.path,req.protocol);
-    if (strcmp(req.path, "/") == 0) {
-        parse_http("index.html", client_sock);
-    }
-    else if (strcmp(req.path, "/about/") == 0) {
-        parse_http("about.html", client_sock);
-    }
-    else if (strcmp(req.path, "/Books/books.html") == 0) {
-        parse_http("Books/books.html", client_sock);
-    }
-    else if (strcmp(req.path, "/Books/book1.html") == 0) {
-        parse_http("Books/book1.html", client_sock);
-    }
-    else if (strcmp(req.path, "/Books/book2.html") == 0) {
-        parse_http("Books/book2.html", client_sock);
-    }
-    else if (strcmp(req.path, "/images/images.html") == 0) {
-        parse_http("images/images.html", client_sock);
-    }
-    else if (strcmp(req.path, "/images/image1.html") == 0) {
-        parse_http("images/image1.html", client_sock);
-    }
-    else if (strcmp(req.path, "/images/image2.html") == 0) {
-        parse_http("images/image2.html", client_sock);
-    }
-    else if (strcmp(req.path, "/favicon.ico") == 0){
-        char buffer[BUFSIZ] = "HTTP/1.1 200 OK\nContent-Type: image/png\n\n";
-    int readsiz = strlen(buffer);
-    send(client_sock, buffer, readsiz, 0);
-    FILE *page = fopen("images/favicon.png", "r");
-    while((readsiz = fread(buffer, sizeof(char), BUFSIZ, page))!=0){
-        send(client_sock, buffer, readsiz, 0);
-    }
-    fclose(page);
-    }
-    else {
-        char buffer[BUFSIZ] = "HTTP/1.1 404 OK\n\n";
-        int readsiz = strlen(buffer);
-        send(client_sock, buffer, readsiz, 0);
-        FILE *page = fopen("page404.html", "r");
-        while((readsiz = fread(buffer, sizeof(char), BUFSIZ, page))!=0){
-            send(client_sock, buffer, readsiz, 0);
+        if ((client_sock = accept(server_sock, (struct sockaddr *)&server_address, &len)) == -1) {
+            perror("accept() error!");
+            exit(-1);
         }
-        fclose(page);
-    }
-    close(client_sock);
+
+        recv(client_sock, recv_msg, sizeof(recv_msg), 0);
+        HTTPreq req = new_http();
+        parse_request(recv_msg, &req);
+        printf("%s %s %s\n", req.method, req.path, req.protocol);
+        if (strcmp(req.path, "/") == 0) {
+            parse_http("index.html", client_sock);
+        } else if (strcmp(req.path, "/about/") == 0) {
+            parse_http("about.html", client_sock);
+        } else if (strcmp(req.path, "/Books/books.html") == 0) {
+            parse_http("Books/books.html", client_sock);
+        } else if (strcmp(req.path, "/Books/book1.html") == 0) {
+            parse_http("Books/book1.html", client_sock);
+        } else if (strcmp(req.path, "/Books/book2.html") == 0) {
+            parse_http("Books/book2.html", client_sock);
+        } else if (strcmp(req.path, "/images/images.html") == 0) {
+            parse_http("images/images.html", client_sock);
+        } else if (strcmp(req.path, "/images/image1.html") == 0) {
+            parse_http("images/image1.html", client_sock);
+        } else if (strcmp(req.path, "/images/image2.html") == 0) {
+            parse_http("images/image2.html", client_sock);
+        } else if (strcmp(req.path, "/favicon.ico") == 0) {
+            char buffer[BUFSIZ] = "HTTP/1.1 200 OK\nContent-Type: image/png\n\n";
+            int readsiz = strlen(buffer);
+            send(client_sock, buffer, readsiz, 0);
+            FILE *page = fopen("images/favicon.png", "r");
+            while ((readsiz = fread(buffer, sizeof(char), BUFSIZ, page)) != 0) {
+                send(client_sock, buffer, readsiz, 0);
+            }
+            fclose(page);
+        } else {
+            char buffer[BUFSIZ] = "HTTP/1.1 404 OK\n\n";
+            int readsiz = strlen(buffer);
+            send(client_sock, buffer, readsiz, 0);
+            FILE *page = fopen("page404.html", "r");
+            while ((readsiz = fread(buffer, sizeof(char), BUFSIZ, page)) != 0) {
+                send(client_sock, buffer, readsiz, 0);
+            }
+            fclose(page);
+        }
+        close(client_sock);
     }
     return 0;
 }
